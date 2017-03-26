a0e7cbdd24f2c9f405907124875b2a9ac4d717b9
danielsbonnin <danielsbonnin@gmail.com>
2017-03-25 19:48:35 -0500

    added public dir and test homepage

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express')
-var app = express()
-
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+// Prepend the path to public folder to static urls
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
